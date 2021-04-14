4646c57631cb840702606ebe0505d5a0b90fb944
sajadevo <nawabisajadahmad@gmail.com>
2021-04-13 23:55:39 -0700

    Add contact section component for the landing page

---
+++ src/components/landing/ContactSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Title from '../landing/Title';
+import ContactCard from '../landing/ContactCard';
+import Form from '../landing/Form';
+
+export default function ContactSection() {
+    return (
+        <section className="pb-20 relative block bg-gray-50">
+            <div className="container max-w-7xl mx-auto px-4 lg:pt-24">
+                <Title heading="Build something">
+                    Put the potentially record low maximum sea ice extent tihs
+                    year down to low ice. According to the National Oceanic and
+                    Atmospheric Administration, Ted, Scambos.
+                </Title>
+
+                <div className="flex flex-wrap mt-12 justify-center">
+                    <ContactCard icon="stars" title="Excelent Services">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </ContactCard>
+                    <ContactCard icon="insert_chart" title="Grow Your Market">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </ContactCard>
+                    <ContactCard icon="launch" title="Launch Time">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                    </ContactCard>
+                </div>
+
+                <Form />
+            </div>
+        </section>
+    );
+}
