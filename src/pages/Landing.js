bcbf73c7a8404b7e04d6e15b1aec8be67fa38196
sajadevo <nawabisajadahmad@gmail.com>
2021-04-14 00:00:12 -0700

    Add landing page

---
+++ src/pages/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../components/landing/Navbar';
+import Footer from '../components/landing/Footer';
+import Header from '../components/landing/Header';
+import WorkingSection from '../components/landing/WorkingSection';
+import TeamSection from '../components/landing/TeamSection';
+import ContactSection from '../components/landing/ContactSection';
+
+export default function Landing() {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <Header />
+                <WorkingSection />
+                <TeamSection />
+                <ContactSection />
+            </main>
+            <Footer />
+        </>
+    );
+}
