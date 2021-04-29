2c68b86bb484fbee9dcf535eecbf324dfd3b9a97
sajadevo <nawabisajadahmad@gmail.com>
2021-04-29 00:20:57 -0700

    Update profile page header component

--- src/components/profile/Header.js
+++ src/components/profile/Header.js
@@ -4,14 +4,7 @@ import BackgroundImage from '../../img/background-1920x1080.jpg';
 export default function Header() {
     return (
         <section className="relative block" style={{ height: '500px' }}>
-            <div
-                className="absolute top-0 w-full h-full"
-                style={{
-                    background: `linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url(${BackgroundImage})`,
-                    backgroundPosition: 'top',
-                    backgroundSize: 'cover',
-                }}
-            />
+            <div className="bg-profile-background bg-cover bg-center absolute top-0 w-full h-full" />
         </section>
     );
 }
