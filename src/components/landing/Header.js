6ce8a3356b2fdaee2f1a2acdac02273e1e864061
sajadevo <nawabisajadahmad@gmail.com>
2021-04-29 00:15:48 -0700

    Update landing page header component

--- src/components/landing/Header.js
+++ src/components/landing/Header.js
@@ -3,18 +3,9 @@ import H2 from '@material-tailwind/react/Heading2';
 import LeadText from '@material-tailwind/react/LeadText';
 
 export default function Header() {
-    const backgroundImage =
-        'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80';
     return (
         <div className="relative pt-16 pb-32 flex content-center items-center justify-center h-screen">
-            <div
-                className="absolute top-0 w-full h-full"
-                style={{
-                    background: `linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url(${backgroundImage})`,
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover',
-                }}
-            />
+            <div className="bg-landing-background bg-cover bg-center absolute top-0 w-full h-full" />
             <div className="container max-w-8xl relative mx-auto">
                 <div className="items-center flex flex-wrap">
                     <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
