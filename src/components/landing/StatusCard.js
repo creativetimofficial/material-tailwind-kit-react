af85bd377d41b75f507154ab990e9c0d937e3ada
sajadevo <nawabisajadahmad@gmail.com>
2021-04-29 03:09:23 -0700

    Update the bg colors for the status card

--- src/components/landing/StatusCard.js
+++ src/components/landing/StatusCard.js
@@ -5,13 +5,35 @@ import Icon from '@material-tailwind/react/Icon';
 import H6 from '@material-tailwind/react/Heading6';
 import Paragraph from '@material-tailwind/react/Paragraph';
 
+const colors = {
+    blueGray: 'bg-blue-gray-500',
+    gray: 'bg-gray-500',
+    brown: 'bg-brown-500',
+    deepOrange: 'bg-deep-orange-500',
+    orange: 'bg-orange-500',
+    amber: 'bg-amber-500',
+    yellow: 'bg-yellow-600',
+    lime: 'bg-lime-500',
+    lightGreen: 'bg-light-green-500',
+    green: 'bg-green-500',
+    teal: 'bg-teal-500',
+    cyan: 'bg-cyan-500',
+    lightBlue: 'bg-light-blue-500',
+    blue: 'bg-blue-500',
+    indigo: 'bg-indigo-500',
+    deepPurple: 'bg-deep-purple-500',
+    purple: 'bg-purple-500',
+    pink: 'bg-pink-500',
+    red: 'bg-red-500',
+};
+
 export default function StatusCard({ color, icon, title, children }) {
     return (
         <div className="w-full md:w-4/12 px-4 flex justify-center text-center">
             <Card>
                 <CardBody>
                     <div
-                        className={`p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-md rounded-full bg-${color}-500`}
+                        className={`p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-md rounded-full ${colors[color]}`}
                     >
                         <Icon name={icon} size="xl" color="white" />
                     </div>
