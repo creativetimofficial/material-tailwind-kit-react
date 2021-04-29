c01b13091020f082920bf55d5a9ca18a1abd200b
sajadevo <nawabisajadahmad@gmail.com>
2021-04-28 23:59:24 -0700

    Add the background image class name for the navbar page component

--- src/components/login/Page.js
+++ src/components/login/Page.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import BackgroundImage from '../../img/background-1920x1280.jpg';
 
 export default function Page({ children }) {
     return (
-        <div
-            className="w-screen h-screen relative flex flex-col justify-between"
-            style={{
-                background: `linear-gradient(rgba(0,0,0, 0.7), rgba(0,0,0, 0.7)), url(${BackgroundImage})`,
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-            }}
-        >
+        <div className="bg-login-background bg-cover bg-center w-screen h-screen relative flex flex-col justify-between">
             {children}
         </div>
     );
