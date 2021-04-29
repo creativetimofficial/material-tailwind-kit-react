47bb853c348e46fdb58e61716ef76002200fb669
sajadevo <nawabisajadahmad@gmail.com>
2021-04-29 00:16:51 -0700

    Add landing page header background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
             backgroundImage: () => ({
                 'login-background':
                     "linear-gradient(rgba(0,0,0, 0.75), rgba(0,0,0, 0.75)), url('/src/assets/img/background-1920x1280.jpg')",
+                'landing-background':
+                    'linear-gradient(rgba(0,0,0, 0.75), rgba(0,0,0, 0.75)), url(https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80)',
             }),
         },
     },
