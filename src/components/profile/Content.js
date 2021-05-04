b24b0aae553ca88e687b223a99a9998f2340b8ac
sajadevo <nawabisajadahmad@gmail.com>
2021-05-04 01:52:33 -0700

    Update contact component

--- src/components/profile/Content.js
+++ src/components/profile/Content.js
@@ -4,7 +4,7 @@ import Image from '@material-tailwind/react/Image';
 import H3 from '@material-tailwind/react/Heading3';
 import Icon from '@material-tailwind/react/Icon';
 import LeadText from '@material-tailwind/react/LeadText';
-import ProfilePicture from '../../img/team-2-800x800.jpg';
+import ProfilePicture from '../../assets/img/team-2-800x800.jpg';
 
 export default function Content() {
     return (
