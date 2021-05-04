ae0873a422e877093073b33e283173f57abea2fa
sajadevo <nawabisajadahmad@gmail.com>
2021-05-04 01:53:12 -0700

    Update the index page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './assets/styles/index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'assets/styles/tailwind.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
     <React.StrictMode>
