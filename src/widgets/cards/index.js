a1098326df58e5894c24e0566c55610426deb0fd
sajadevo <nawabisajadahmad@gmail.com>
2022-11-03 13:58:03 +0430

    add one export points for widgets

---
+++ src/widgets/cards/index.js
@@ -0,0 +1,2 @@
+export * from "@/widgets/cards/feature-card";
+export * from "@/widgets/cards/team-card";
---
+++ src/widgets/layout/index.js
@@ -0,0 +1,4 @@
+export * from "@/widgets/layout/navbar";
+export * from "@/widgets/layout/footer";
+export * from "@/widgets/layout/simple-footer";
+export * from "@/widgets/layout/page-title";
