d81bcb673247f35bb664aa9c56b25feace955afd
sajadevo <nawabisajadahmad@gmail.com>
2022-11-03 13:57:23 +0430

    add team card

---
+++ src/widgets/cards/team-card.jsx
@@ -0,0 +1,40 @@
+import PropTypes from "prop-types";
+import { Card, Avatar, Typography } from "@material-tailwind/react";
+
+export function TeamCard({ img, name, position, socials }) {
+  return (
+    <Card color="transparent" shadow={false} className="text-center">
+      <Avatar
+        src={img}
+        alt={name}
+        size="xxl"
+        className="h-full w-full shadow-lg shadow-gray-500/25"
+      />
+      <Typography variant="h5" color="blue-gray" className="mt-6 mb-1">
+        {name}
+      </Typography>
+      {position && (
+        <Typography className="font-normal text-blue-gray-500">
+          {position}
+        </Typography>
+      )}
+      {socials && <div className="mx-auto mt-5">{socials}</div>}
+    </Card>
+  );
+}
+
+TeamCard.defaultProps = {
+  position: "",
+  socials: null,
+};
+
+TeamCard.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  position: PropTypes.string,
+  socials: PropTypes.node,
+};
+
+TeamCard.displayName = "/src/widgets/layout/team-card.jsx";
+
+export default TeamCard;
