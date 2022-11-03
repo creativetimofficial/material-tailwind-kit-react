d5f1f21e333c185a0e0f6f47fef64aa993fd980d
sajadevo <nawabisajadahmad@gmail.com>
2022-11-03 13:57:34 +0430

    add footer

---
+++ src/widgets/layout/footer.jsx
@@ -0,0 +1,180 @@
+import PropTypes from "prop-types";
+import { Typography, IconButton } from "@material-tailwind/react";
+
+const year = new Date().getFullYear();
+
+export function Footer({ title, description, socials, menus, copyright }) {
+  return (
+    <footer className="relative pt-8 pb-6 px-4">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap text-center lg:text-left pt-6">
+          <div className="w-full lg:w-6/12 px-4">
+            <Typography variant="h4" className="mb-4" color="blue-gray">
+              {title}
+            </Typography>
+            <Typography className="text-blue-gray-500 font-normal">
+              {description}
+            </Typography>
+            <div className="flex gap-2 mt-6 lg:justify-start md:mb-0 mb-8 justify-center mx-auto">
+              {socials.map(({ color, name, path }) => (
+                <a
+                  key={name}
+                  href={path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IconButton color="white" className="rounded-full">
+                    <Typography color={color}>
+                      <i className={`fa-brands fa-${name}`} />
+                    </Typography>
+                  </IconButton>
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="w-max mx-auto grid grid-cols-2 gap-24 mt-12 lg:mt-0">
+            {menus.map(({ name, items }) => (
+              <div key={name}>
+                <Typography
+                  variant="small"
+                  color="blue-gray"
+                  className="block uppercase font-medium mb-2"
+                >
+                  {name}
+                </Typography>
+                <ul className="mt-3">
+                  {items.map((item) => (
+                    <li key={item.name}>
+                      <Typography
+                        as="a"
+                        href={item.path}
+                        target="_blank"
+                        rel="noreferrer"
+                        variant="small"
+                        className="text-blue-gray-500 hover:text-blue-gray-700 block mb-2 font-normal"
+                      >
+                        {item.name}
+                      </Typography>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+        <hr className="my-6 border-gray-300" />
+        <div className="flex flex-wrap items-center md:justify-between justify-center">
+          <div className="w-full px-4 mx-auto text-center">
+            <Typography
+              variant="small"
+              className="font-normal text-blue-gray-500"
+            >
+              {copyright}
+            </Typography>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.defaultProps = {
+  title: "Material Tailwind",
+  description:
+    "Easy to use React components for Tailwind CSS and Material Design.",
+  socials: [
+    {
+      color: "blue",
+      name: "facebook",
+      path: "https://www.facebook.com/CreativeTim",
+    },
+    {
+      color: "light-blue",
+      name: "twitter",
+      path: "https://www.twitter.com/creativetim",
+    },
+    {
+      color: "purple",
+      name: "instagram",
+      path: "https://www.instagram.com/creativetimofficial/",
+    },
+    {
+      color: "pink",
+      name: "dribbble",
+      path: "https://www.dribbble.com/creativetim",
+    },
+    {
+      color: "red",
+      name: "youtube",
+      path: "https://www.youtube.com/channel/UCVyTG4sCw-rOvB9oHkzZD1w",
+    },
+    {
+      color: "black",
+      name: "github",
+      path: "https://github.com/creativetimofficial/material-tailwind",
+    },
+  ],
+  menus: [
+    {
+      name: "useful links",
+      items: [
+        { name: "About Us", path: "https://www.creative-tim.com/presentation" },
+        { name: "Blog", path: "https://www.creative-tim.com/blog" },
+        {
+          name: "Github",
+          path: "https://www.github.com/creativetimofficial/material-tailwind?ref=mtk",
+        },
+        {
+          name: "Free Products",
+          path: "https://www.creative-tim.com/templates/free?ref=mtk",
+        },
+      ],
+    },
+    {
+      name: "other resources",
+      items: [
+        {
+          name: "MIT License",
+          path: "https://github.com/creativetimofficial/material-tailwind/blob/main/LICENSE.md?ref=mtk",
+        },
+        {
+          name: "Contribute",
+          path: "https://github.com/creativetimofficial/material-tailwind/blob/main/CONTRIBUTING.md?ref=mtk",
+        },
+        {
+          name: "Change Log",
+          path: "https://github.com/creativetimofficial/material-tailwind/blob/main/CHANGELOG.md?ref=mtk",
+        },
+        {
+          name: "Contact Us",
+          path: "https://creative-tim.com/contact-us?ref=mtk",
+        },
+      ],
+    },
+  ],
+  copyright: (
+    <>
+      Copyright © {year} Material Tailwind by{" "}
+      <a
+        href="https://www.creative-tim.com?ref=mtk"
+        target="_blank"
+        className="text-blue-gray-500 transition-colors hover:text-blue-500"
+      >
+        Creative Tim
+      </a>
+      .
+    </>
+  ),
+};
+
+Footer.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  socials: PropTypes.arrayOf(PropTypes.object),
+  menus: PropTypes.arrayOf(PropTypes.object),
+  copyright: PropTypes.node,
+};
+
+Footer.displayName = "/src/widgets/layout/footer.jsx";
+
+export default Footer;
