6c4a3ac4195e41f759969ec3d4551c45c65e5878
sajadevo <nawabisajadahmad@gmail.com>
2022-11-10 15:52:25 +0430

    ready for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -13,16 +13,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import "../public/css/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/material-tailwind-kit-react/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
