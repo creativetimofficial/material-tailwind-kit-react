ad0db15b9869396af4e97c508b588c8ea05c7730
sajadevo <nawabisajadahmad@gmail.com>
2023-11-14 19:02:56 +0430

    update profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -10,13 +10,13 @@ export function Profile() {
   return (
     <>
       <section className="relative block h-[50vh]">
-        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/background-3.png')] bg-cover bg-center" />
+        <div className="bg-profile-background absolute top-0 h-full w-full bg-[url('/img/background-3.png')] bg-cover bg-center scale-105" />
         <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
       </section>
       <section className="relative bg-white py-16">
-        <div className="relative mb-6 -mt-40 flex w-full min-w-0 flex-col break-words bg-white">
+        <div className="relative mb-6 -mt-40 flex w-full px-4 min-w-0 flex-col break-words bg-white">
           <div className="container mx-auto">
-            <div className="flex flex-wrap justify-between">
+            <div className="flex flex-col lg:flex-row justify-between">
               <div className="relative flex gap-6 items-start">
                 <div className="-mt-20 w-40">
                   <Avatar
@@ -30,12 +30,12 @@ export function Profile() {
                   <Typography variant="h4" color="blue-gray">
                     Jenna Stones
                   </Typography>
-                  <Typography variant="paragraph" color="gray" className="!mt-0">jena@mail.com</Typography>
+                  <Typography variant="paragraph" color="gray" className="!mt-0 font-normal">jena@mail.com</Typography>
                 </div>
               </div>
 
-              <div className="mt-10 flex flex-col justify-end px-4 lg:-mt-5">
-                <Button className="bg-gray-900 w-fit ml-auto">Conntect</Button>
+              <div className="mt-10 mb-10 flex lg:flex-col justify-between items-center lg:justify-end lg:mb-0 lg:px-4 flex-wrap lg:-mt-5">
+                <Button className="bg-gray-900 w-fit lg:ml-auto">Conntect</Button>
                 <div className="flex justify-start py-4 pt-8 lg:pt-4">
                   <div className="mr-4 p-3 text-center">
                     <Typography
